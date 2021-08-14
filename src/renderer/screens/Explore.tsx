82a158217d59a05d71987a574eb13eff1221f7ca
tharinda <tharindahp@gmail.com>
2021-08-14 20:25:47 +0530

    renamed Home->Explore

---
+++ src/renderer/screens/Explore.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+import "../screens/Explore.css";
+
+const Explore = () => {
+  return (
+    <div className="explore">
+      <h1>Explore</h1>
+    </div>
+  );
+};
+
+export default Explore;
