1a7d6928b5f32b2a93d3c9b4a2c2b8854e2009a9
tharinda <tharindahp@gmail.com>
2021-08-14 20:34:23 +0530

    added id attribute

--- src/shared/data/sidebarData.ts
+++ src/shared/data/sidebarData.ts
@@ -1,11 +1,12 @@
 import * as FaIcon from "react-icons/fa"
 import SidebarTabProps from "../types/sidebarTabProps";
 
-const TAB_CLASS_NAME = "sidebar-tab"
+const TAB_CLASS_NAME = "tab"
 const ICON_COLOR = "white";
 const ICON_SIZE = 24;
 
 export const Home:SidebarTabProps = {
+  id: "explore",
   cName: TAB_CLASS_NAME,
   title: "Explore",
   path: "/",
