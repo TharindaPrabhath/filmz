c6727c050cd01516086789e1f403eac311cfd5a7
tharinda <tharindahp@gmail.com>
2021-08-14 20:25:20 +0530

    moved interface to another new file

--- src/shared/data/sidebarData.ts
+++ src/shared/data/sidebarData.ts
@@ -1,61 +1,21 @@
-import * as AiIcon from "react-icons/ai";
-import { IconType } from "react-icons/lib";
+import * as FaIcon from "react-icons/fa"
+import SidebarTabProps from "../types/sidebarTabProps";
 
 const TAB_CLASS_NAME = "sidebar-tab"
 const ICON_COLOR = "white";
 const ICON_SIZE = 24;
 
-interface SidebarTabProps {
-  cName:string;
-  title:string;
-  path:string;
-  icon:{
-    name:IconType;
-    color:string;
-    size:number;
-  };
-}
-
 export const Home:SidebarTabProps = {
   cName: TAB_CLASS_NAME,
-  title: "Home",
+  title: "Explore",
   path: "/",
   icon:{
-    name:AiIcon.AiFillHome,
+    name:FaIcon.FaRegCompass,
     color: ICON_COLOR,
     size: ICON_SIZE
   }
 }
 
-export const Store:SidebarTabProps = {
-  cName: TAB_CLASS_NAME,
-  title: "Store",
-  path: "/store",
-  icon:{
-    name:AiIcon.AiFillShop,
-    color: ICON_COLOR,
-    size: ICON_SIZE
-  }
-}
-
-export const Settings:SidebarTabProps = {
-  cName: TAB_CLASS_NAME,
-  title: "Settings",
-  path: "/settings",
-  icon:{
-    name:AiIcon.AiFillSetting,
-    color: ICON_COLOR,
-    size: ICON_SIZE
-  }
-}
-
-export const About:SidebarTabProps = {
-  cName: TAB_CLASS_NAME,
-  title: "About",
-  path: "/about",
-  icon:{
-    name:AiIcon.AiFillInfoCircle,
-    color: ICON_COLOR,
-    size: ICON_SIZE
-  }
-}
+export const tabs:SidebarTabProps[] = [
+  Home
+]
