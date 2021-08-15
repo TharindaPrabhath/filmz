eabcbb509936ec1ca48bec075b441d86af009ab4
tharinda <tharindahp@gmail.com>
2021-08-15 19:48:35 +0530

    created

---
+++ src/shared/data/tmdb/meta.ts
@@ -0,0 +1,3 @@
+export const API_KEY = "d1930fef0ac94459c96758fa16af0a62";
+export const BASE_URL = "https://api.themoviedb.org/3";
+export const IMAGE_BASE_URL = "https://image.tmdb.org/t/p/original";
