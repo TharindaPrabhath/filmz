74919d0b344dbefb74e6201a5959b42ea376b461
tharinda <tharindahp@gmail.com>
2021-08-15 19:48:26 +0530

    added slider component

--- src/renderer/screens/Explore.tsx
+++ src/renderer/screens/Explore.tsx
@@ -1,11 +1,33 @@
 import React from "react";
+import { useEffect } from "react";
+import Slider from "../components/Slider";
 
 import "../screens/Explore.css";
 
+import axios from "../../shared/utils/axios";
+import requests from "../../shared/data/tmdb/requests";
+
+import { useState } from "react";
+import Film from "../../shared/types/film";
+
 const Explore = () => {
+  const [trendings, setTrendings] = useState<Film[]>([]);
+
+  useEffect(() => {
+    async function fetchTrending() {
+      const res = await axios.get(requests.fetchTrending);
+      setTrendings(res.data.results as Film[]);
+      return res;
+    }
+
+    fetchTrending();
+  }, [requests.fetchTrending]);
+
+  //console.log(trendings[0]?.adult);
+
   return (
     <div className="explore">
-      <h1>Explore</h1>
+      <Slider films={trendings.slice(0, 5)} />
     </div>
   );
 };
