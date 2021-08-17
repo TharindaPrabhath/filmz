3820b081502770611f94da9e2b8e0e702f52cd85
tharinda <tharindahp@gmail.com>
2021-08-17 19:15:33 +0530

    reformatted props

--- src/renderer/components/CardRow.tsx
+++ src/renderer/components/CardRow.tsx
@@ -8,49 +8,24 @@ import SlickSlider from "./SlickSlider";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 
-const CardRow = ({ title, films }: { title?: string; films: Film[] }) => {
-  const slickSliderConfig = {
-    dots: true,
-    infinite: true,
-    slidesToShow: 4,
-    slidesToScroll: 3,
-    speed: 500,
-    cssEase: "linear",
-    responsive: [
-      {
-        breakpoint: 1024,
-        settings: {
-          slidesToShow: 3,
-          slidesToScroll: 3,
-          infinite: true,
-          dots: true,
-        },
-      },
-      {
-        breakpoint: 900,
-        settings: {
-          slidesToShow: 2,
-          slidesToScroll: 2,
-          initialSlide: 2,
-        },
-      },
-      {
-        breakpoint: 480,
-        settings: {
-          slidesToShow: 1,
-          slidesToScroll: 1,
-        },
-      },
-    ],
-  };
-
+const CardRow = ({
+  title,
+  films,
+  slickSliderConfig,
+  cardSize,
+}: {
+  title?: string;
+  films: Film[];
+  slickSliderConfig: any;
+  cardSize: string;
+}) => {
   return (
     <div className="card-row">
       <div className="card-row__content">
         <h3 className="title">{title}</h3>
         <SlickSlider {...slickSliderConfig}>
           {films.map((film, index) => {
-            return <Card key={index} film={film} isLarge={false} />;
+            return <Card key={index} film={film} size={cardSize} />;
           })}
         </SlickSlider>
       </div>
