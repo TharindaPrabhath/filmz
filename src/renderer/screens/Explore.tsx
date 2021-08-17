1ee72839481e7b5f25f740451718a77b209cceeb
tharinda <tharindahp@gmail.com>
2021-08-17 19:15:09 +0530

    added most populars

--- src/renderer/screens/Explore.tsx
+++ src/renderer/screens/Explore.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import { useEffect } from "react";
 import Slider from "../components/Slider";
-import SlickSlider from "react-slick";
 
 import "../screens/Explore.css";
 
@@ -11,9 +10,17 @@ import requests from "../../shared/data/tmdb/requests";
 import { useState } from "react";
 import Film from "../../shared/types/film";
 import CardRow from "../components/CardRow";
+import {
+  centeredSlide,
+  multipleSlides,
+  singleSlide,
+} from "../../shared/data/slickSliderConfigs";
+import Size from "../../shared/types/size";
+import Card from "../components/Card";
 
 const Explore = () => {
   const [trendings, setTrendings] = useState<Film[]>([]);
+  const [mostPopulars, setMostPopulars] = useState<Film[]>([]);
 
   useEffect(() => {
     async function fetchTrending() {
@@ -22,15 +29,40 @@ const Explore = () => {
       return res;
     }
 
+    async function fetchMostPopular() {
+      const res = await axios.get(requests.fetchMostPopular);
+      setMostPopulars(res.data.results as Film[]);
+      return res;
+    }
+
     fetchTrending();
-  }, [requests.fetchTrending]);
+    fetchMostPopular();
+  }, [requests.fetchTrending, requests.fetchMostPopular]);
 
   return (
     <div className="explore">
       <div className="explore__content">
         <Slider films={trendings.slice(0, 5)} />
-        <CardRow title="New Arrivals" films={trendings.slice(6, 16)} />
-        <CardRow title="New Arrivals" films={trendings.slice(6, 16)} />
+        <CardRow
+          title="New Arrivals"
+          films={trendings.slice(6, 16)}
+          slickSliderConfig={multipleSlides}
+          cardSize={Size.SMALL}
+        />
+
+        <div className="most-popular">
+          <CardRow
+            films={mostPopulars.slice(0, 5)}
+            title="Most Popular"
+            slickSliderConfig={centeredSlide}
+            cardSize={Size.EXTRA_LARGE}
+          />
+        </div>
+
+        <h1>________</h1>
+        <h1>________</h1>
+        <h1>________</h1>
+        <h1>________</h1>
       </div>
     </div>
   );
