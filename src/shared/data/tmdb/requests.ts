9a9116dc4a54572e882edd0bd79342f926d487b9
tharinda <tharindahp@gmail.com>
2021-08-17 21:23:47 +0530

    removed upcoming request

--- src/shared/data/tmdb/requests.ts
+++ src/shared/data/tmdb/requests.ts
@@ -5,8 +5,7 @@ const requests = {
     fetchTopRated: `/movie/top_rated?api_key=${API_KEY}&language=em-US`,
     fetchMostPopular: `/discover/movie?api_key=${API_KEY}&sort_by=popularity.desc`,
     fetchNowPlaying: `/movie/now_playing?api_key=${API_KEY}&language=en-US&page=1`,
-    fetchNewArrivals: `/movie/latest?api_key=${API_KEY}&language=en-US`,
-    fetchUpcoming: `/movie/upcoming?api_key=${API_KEY}&language=en-US&page=1`,
+    fetchNewArrivals: `/movie/upcoming?api_key=${API_KEY}&language=en-US&page=1`,
 }
 
 export default requests;
